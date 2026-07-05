75fa2842aed00caa34ec4f2198d2889dc1b89f74
Timothy <timo.dev96@gmail.com>
2026-07-05 07:24:12 +0300

    added the main pipe spawning system and useful debugging messages

--- main.py
+++ main.py
@@ -3,20 +3,38 @@ import sys
 import random 
 from pygame.math import Vector2 as V2
 
+#Grid & Screen Settings
+cell_size = 45
+cell_number =22
+
+#Pipe settings
+pipe_speed = 2
+top_bottom_pipe_space = 4
+pipe_to_pipe_space = 500
+
+#Colors
+BG_COLOR = (10,23,250)
+SNAKE_COLOR = (183,111,122)
+FRUIT_COLOR = (194, 54, 22)
+PIPE_COLOR = (34,139,30)
+
 class SNAKE:
     def __init__(self):
         self.body = [V2(5,10),V2(6,10),V2(7,10)]
         self.direction = V2(1,0)
+
     def draw_snake(self):
-        for blcok in self.body:
-            x_pos = int(blcok.x*cell_size)
-            y_pos = int(blcok.y*cell_size)
+        for block in self.body:
+            x_pos = int(block.x*cell_size)
+            y_pos = int(block.y*cell_size)
             snake_rect = pygame.Rect(x_pos,y_pos,cell_size,cell_size)
-            pygame.draw.rect(screen,(183,111,122),snake_rect)
+            pygame.draw.rect(screen,SNAKE_COLOR,snake_rect)
+
     def move_snake(self):
         body_copy = self.body[:-1]
         body_copy.insert(0,body_copy[0]+self.direction)
         self.body = body_copy[:]
+
 class FRUIT:
     def __init__(self):
         self.x  = random.randint(0,cell_number-1)
@@ -25,18 +43,85 @@ class FRUIT:
 
     def draw_fruit(self):
         fruit_rect = pygame.Rect(self.pos.x * cell_size,self.pos.y*cell_size,cell_size,cell_size)
-        pygame.draw.rect(screen,(126,155,123),fruit_rect)
+        pygame.draw.rect(screen,FRUIT_COLOR,fruit_rect)
+
+class PIPES:
+    def __init__(self):
+        self.pipes_list=[]
+        self.pipe_gap= top_bottom_pipe_space
+        self.pipe_speed = pipe_speed
+        self.spawn_pipe()
+
+    def spawn_pipe(self):
+        x_pixel_pos = cell_number*cell_size
+        top_pipe_end = random.randint(2, cell_number-self.pipe_gap-2)
+        bottom_pipe_start = top_pipe_end+self.pipe_gap
+        new_pipe = {
+            'x':x_pixel_pos,
+            'top_end':top_pipe_end,
+            'bottom_start':bottom_pipe_start,
+            'spawned_next': False
+        }
+        self.pipes_list.append(new_pipe)
+        print(f'[Debug] new pipe at x={x_pixel_pos}, Gap:(Y1={top_pipe_end} Y2={bottom_pipe_start}) ')
+
+    def move_pipes(self):
+        for pipe in self.pipes_list:
+            pipe['x']-= self.pipe_speed
+        old_count = len(self.pipes_list)
+        self.pipes_list= [pipe for pipe in self.pipes_list if pipe['x']>=-cell_size]
+
+        if len(self.pipes_list)< old_count:
+            print(f'[Debug] Out of screen pips had removed')
+
+    def draw_pipes(self):
+        if len(self.pipes_list)== 0:
+            print(f'[Debug] Pipe List is empty!')
+
+        for pipe in self.pipes_list:
+
+            for y in range(0, pipe['top_end']):
+                pipe_rect = pygame.Rect(pipe['x'], y*cell_size , cell_size , cell_size)
+                pygame.draw.rect(screen,(34,139,34), pipe_rect)
+
+            for y in range(pipe['bottom_start'],cell_number):
+                pipe_rect = pygame.Rect(pipe['x'], y*cell_size , cell_size , cell_size)
+                pygame.draw.rect(screen, PIPE_COLOR, pipe_rect)
+    def check_and_spawn(self):
+        if len(self.pipes_list) > 0:
+            last_pipe = self.pipes_list[-1]
+            screen_width_pixels = cell_number * cell_size
+        else:
+            self.spawn_pipe()
+
+        if last_pipe['x'] <= screen_width_pixels - pipe_to_pipe_space and not last_pipe['spawned_next']:
+            last_pipe['spawned_next'] = True
+            self.spawn_pipe()
+        
 class MAIN:
     def __init__(self):
         self.snake = SNAKE()
         self.fruit = FRUIT()
+        self.pipes = PIPES()
+        self.snake_timer = 0
+
     def update(self):
-        self.snake.move_snake()
-        self.check_snake_fruit_collision()
+        self.pipes.move_pipes()
+        self.pipes.check_and_spawn()
+
+        self.snake_timer += 1
+        if self.snake_timer >= 9:
+            self.snake.move_snake()
+            self.check_snake_fruit_collision()
+            self.check_snake_pipe_collision()
+            self.snake_timer=0
+
     def draw_elemnts(self):
         self.fruit.draw_fruit()
         self.snake.draw_snake()
-    def inputs(self):
+        self.pipes.draw_pipes()
+
+    def inputs(self,event):
         if event.key == pygame.K_w or event.key == pygame.K_UP:
             self.snake.direction = V2(0,-1)
         if event.key == pygame.K_s or event.key == pygame.K_DOWN:
@@ -45,36 +130,46 @@ class MAIN:
             self.snake.direction = V2(-1,0)
         if event.key == pygame.K_d or event.key == pygame.K_RIGHT:
             self.snake.direction = V2(1,0)
+
     def check_snake_fruit_collision(self):
         if self.fruit.pos == self.snake.body[0]:
-            print("Snake & Fruit collided") # Debug
+            print(f'[Debug] Snake and apple Colided')
+
+    def check_snake_pipe_collision(self):
+        snake_head = self.snake.body[0]
+
+        for pipe in self.pipes.pipes_list:
+            pipe_grid_x = pipe['x'] // cell_size
+            if snake_head.x == pipe_grid_x:
+                if snake_head.y < pipe['top_end']:
+                    print("[Debug] Snake collided with TOP pipe!")
+                elif snake_head.y >= pipe['bottom_start']:
+                    print("[Debug] Snake collided with BOTTOM pipe!")
+
+    def check_if_game_close(self):
+        for event in pygame.event.get(): 
+            if event.type == pygame.QUIT:
+                pygame.quit()
+                sys.exit()
+            if event.type == SCREEN_UPDATE:
+                self.update()
+            if event.type == pygame.KEYDOWN:
+                self.inputs(event)
+
+
 pygame.init()
 
-cell_size = 40
-cell_number =20
 screen = pygame.display.set_mode((cell_number*cell_size,cell_number*cell_size))
 clock = pygame.time.Clock()
 
-
-
 SCREEN_UPDATE = pygame.USEREVENT
-pygame.time.set_timer(SCREEN_UPDATE,150)
+pygame.time.set_timer(SCREEN_UPDATE,16)
 
 main_game = MAIN()
 
 while True:
-    for event in pygame.event.get(): 
-        if event.type == pygame.QUIT:
-            pygame.quit()
-            sys.exit()
-
-        if event.type == SCREEN_UPDATE:
-            main_game.update()
-        if event.type == pygame.KEYDOWN:
-            main_game.inputs()
-            
-            
-    screen.fill((10,23,250))
+    main_game.check_if_game_close()
+    screen.fill(BG_COLOR)
     main_game.draw_elemnts()
     pygame.display.update()
     clock.tick(60)
