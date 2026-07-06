aebea21f120014e7090c21d610ad1651e2cd9b3e
Timothy <timo.dev96@gmail.com>
2026-07-06 14:52:38 +0300

    Adding the main Graphics
    snake and pipes logic

--- main.py
+++ main.py
@@ -28,13 +28,73 @@ class SNAKE:
         self.direction = V2(1,0)
         self.last_moved_direction = V2(1,0)
         self.new_block = False
+        
+        self.head_up = pygame.image.load('Graphics/head_up.png').convert_alpha()
+        self.head_down = pygame.image.load('Graphics/head_down.png').convert_alpha()
+        self.head_right = pygame.image.load('Graphics/head_right.png').convert_alpha()
+        self.head_left = pygame.image.load('Graphics/head_left.png').convert_alpha()
+        self.tail_up = pygame.image.load('Graphics/tail_up.png').convert_alpha()
+        self.tail_down = pygame.image.load('Graphics/tail_down.png').convert_alpha()
+        self.tail_right = pygame.image.load('Graphics/tail_right.png').convert_alpha()
+        self.tail_left = pygame.image.load('Graphics/tail_left.png').convert_alpha()
+        self.body_vertical = pygame.image.load('Graphics/body_vertical.png').convert_alpha()
+        self.body_horizontal = pygame.image.load('Graphics/body_horizontal.png').convert_alpha()
+        self.body_tr = pygame.image.load('Graphics/body_tr.png').convert_alpha()
+        self.body_tl = pygame.image.load('Graphics/body_tl.png').convert_alpha()
+        self.body_br = pygame.image.load('Graphics/body_br.png').convert_alpha()
+        self.body_bl = pygame.image.load('Graphics/body_bl.png').convert_alpha()
 
     def draw_snake(self):
-        for block in self.body:
-            x_pos = int(block.x*cell_size)
-            y_pos = int(block.y*cell_size)
-            snake_rect = pygame.Rect(x_pos,y_pos,cell_size,cell_size)
-            pygame.draw.rect(screen,SNAKE_COLOR,snake_rect)
+        self.update_head_graphics()
+        self.update_tail_graphics()
+
+        for index,block in enumerate(self.body):
+            x_pos = int(block.x * cell_size)
+            y_pos = int(block.y * cell_size)
+            block_rect = pygame.Rect(x_pos,y_pos,cell_size,cell_size)
+            
+            if index== 0:
+                screen.blit(self.head,block_rect)
+            elif index == len(self.body) -1:
+                screen.blit(self.tail,block_rect)
+            else:
+                previous_block = self.body[index+1] - block
+                next_block = self.body[index-1] - block
+                if previous_block.x == next_block.x:
+                    screen.blit(self.body_vertical,block_rect)
+                elif previous_block.y == next_block.y:
+                    screen.blit(self.body_horizontal,block_rect)
+                else:
+                    if previous_block.x == -1 and next_block.y == -1 or previous_block.y == -1 and next_block.x == -1:
+                        screen.blit(self.body_tl,block_rect)
+                    elif previous_block.x == -1 and next_block.y == 1 or previous_block.y == 1 and next_block.x == -1:
+                        screen.blit(self.body_bl,block_rect)
+                    elif previous_block.x == 1 and next_block.y == -1 or previous_block.y == -1 and next_block.x == 1:
+                        screen.blit(self.body_tr,block_rect)
+                    elif previous_block.x == 1 and next_block.y == 1 or previous_block.y == 1 and next_block.x == 1:
+                        screen.blit(self.body_br,block_rect)
+
+    def update_head_graphics(self):
+        head_relation = self.body[1] - self.body[0]
+        if head_relation == V2(1,0):
+            self.head = self.head_left
+        elif head_relation == V2(-1,0):
+            self.head = self.head_right
+        elif head_relation == V2(0,1):
+            self.head = self.head_up
+        elif head_relation == V2(0,-1):
+            self.head = self.head_down
+
+    def update_tail_graphics(self):
+        tail_relation = self.body[-2] - self.body[-1]
+        if tail_relation == V2(1,0):
+            self.tail = self.tail_left
+        elif tail_relation == V2(-1,0):
+            self.tail = self.tail_right
+        elif tail_relation == V2(0,1):
+            self.tail = self.tail_up
+        elif tail_relation == V2(0,-1):
+            self.tail = self.tail_down
 
     def move_snake(self):
         self.last_moved_direction = self.direction
@@ -97,18 +157,24 @@ class PIPES:
             #print(f'[Debug] Out of screen pips had removed')
 
     def draw_pipes(self):
-        if len(self.pipes_list)== 0:
+        if len(self.pipes_list) == 0:
             print(f'[Debug] Pipe List is empty!')
-
         for pipe in self.pipes_list:
-
+            pipe_x = pipe['x']
             for y in range(0, pipe['top_end']):
-                pipe_rect = pygame.Rect(pipe['x'], y*cell_size , cell_size , cell_size)
-                pygame.draw.rect(screen,PIPE_COLOR, pipe_rect)
+                pipe_rect = pygame.Rect(pipe_x, y * cell_size, cell_size, cell_size)
+                if y == pipe['top_end'] - 1:
+                    screen.blit(pipe_rim_top, pipe_rect)
+                else:
+                    screen.blit(pipe_body_sprite, pipe_rect)
+            for y in range(pipe['bottom_start'], cell_number):
+                pipe_rect = pygame.Rect(pipe_x, y * cell_size, cell_size, cell_size)
+                
+                if y == pipe['bottom_start']:
+                    screen.blit(pipe_rim_bottom, pipe_rect)
+                else:
+                    screen.blit(pipe_body_sprite, pipe_rect)
 
-            for y in range(pipe['bottom_start'],cell_number):
-                pipe_rect = pygame.Rect(pipe['x'], y*cell_size , cell_size , cell_size)
-                pygame.draw.rect(screen, PIPE_COLOR, pipe_rect)
     def check_and_spawn(self):
         if len(self.pipes_list) > 0:
             last_pipe = self.pipes_list[-1]
@@ -215,10 +281,31 @@ class MAIN:
                     self.score += pipe_points
                     print(f'[Debug] Snake passed throw a Pipe.. add score {pipe_points}')
 
-
 screen_cords = cell_number * cell_size
 screen = pygame.display.set_mode((screen_cords, screen_cords))
+
 apple = pygame.image.load('Graphics/apple.png').convert_alpha() 
+pipe_raw = pygame.image.load('Graphics/pipe.png').convert_alpha()
+pipe_w = pipe_raw.get_width()
+pipe_h = pipe_raw.get_height()
+rim_height = int(pipe_h * 0.25)
+pipe_rim_raw = pipe_raw.subsurface(pygame.Rect(0, 0, pipe_w, rim_height))
+pipe_rim_bottom = pygame.transform.smoothscale(pipe_rim_raw, (cell_size, cell_size))
+
+pipe_rim_top = pygame.transform.flip(pipe_rim_bottom, False, True)
+
+body_height = pipe_h - rim_height
+pipe_body_raw = pipe_raw.subsurface(pygame.Rect(0, rim_height, pipe_w, body_height))
+pipe_body_sprite = pygame.transform.smoothscale(pipe_body_raw, (cell_size, cell_size))
+
+bg_surface_raw = pygame.image.load('Graphics/bglong.png').convert()
+zoomed_width = int(screen_cords * 2)
+zoomed_height = int(screen_cords * 1.5)
+bg_surface = pygame.transform.smoothscale(bg_surface_raw, (zoomed_width, zoomed_height))
+bg_x_pos = 0
+bg_scroll_speed = 1
+
+
 clock = pygame.time.Clock()
 
 SCREEN_UPDATE = pygame.USEREVENT
@@ -228,7 +315,15 @@ main_game = MAIN()
 
 while True:
     main_game.check_if_game_close()
-    screen.fill(BG_COLOR)
+
+    bg_x_pos -= bg_scroll_speed
+    if bg_x_pos <= -zoomed_width:
+        bg_x_pos = 0
+    crop_y = (screen_cords - zoomed_height) + 60
+
+    screen.blit(bg_surface, (bg_x_pos, crop_y))
+    screen.blit(bg_surface, (bg_x_pos + zoomed_width, crop_y))
+
     main_game.draw_elemnts()
     pygame.display.update()
     clock.tick(60)
